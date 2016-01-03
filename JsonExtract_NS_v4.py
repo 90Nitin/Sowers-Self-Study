3a74559627f3bee5cef40897b5b2c54de7ac0a7f
Nitin Srivastava <nitin.iitroorkee@gmail.com>
2016-01-03 13:36:09 +0530

    WIP - Integrating haversine formula to distance calculations

--- JsonExtract_NS_v4.py
+++ JsonExtract_NS_v4.py
@@ -2,8 +2,8 @@ __author__ = 'nsrivas3'
 
 '''
 TTD
-1. Include the Hoversine Formula
-2. Thresholding for Motion Detection
+1. Use the HaverSine formula to compute the distances
+2. Thresholding to compute speed when the workers have actually moved
 '''
 
 import math
@@ -66,39 +66,42 @@ print(json_data)
 print("###################################################################### \n")
 
 for i in  range(0,json_data.__len__()):
-	temp = []
-	if (i>0):
-		if (json_data[i]['worker']==json_data[i-1]['worker']):
-			temp.append(json_data[i]['worker'])
-			delLat = abs(json_data[i]['lat'] - json_data[i-1]['lat'])
-			delLng = abs(json_data[i]['lng'] - json_data[i-1]['lng'])
-			temp.append(math.sqrt(delLat**2+delLng**2))
-			temp.append(json_data[i]['time'])
-		else:
-			temp.append(json_data[i]['worker'])
-			delLat = 0.0
-			delLng = 0.0
-			temp.append(math.sqrt(delLat**2+delLng**2))
-			temp.append(json_data[i]['time'])
-	else:
-		temp.append(1)
-		delLat = 0.0
-		delLng = 0.0
-		temp.append(math.sqrt(delLat**2+delLng**2))
-		temp.append(5)
-	worker_data.append(temp)
+    temp = []
+    if (i>0):
+        if (json_data[i]['worker']==json_data[i-1]['worker']):
+            temp.append(json_data[i]['worker'])
+            temp.append(hvsne("K",json_data[i-1]['lat'],json_data[i-1]['lng'],json_data[i]['lat'],json_data[i]['lng']))
+            temp.append(json_data[i]['time'])
+        else:
+            temp.append(json_data[i]['worker'])
+            temp.append(hvsne("K",json_data[i-1]['lat'],json_data[i-1]['lng'],json_data[i]['lat'],json_data[i]['lng']))
+            temp.append(json_data[i]['time'])
+    else:
+        temp.append(1)
+        temp.append(hvsne("K",json_data[i-1]['lat'],json_data[i-1]['lng'],json_data[i]['lat'],json_data[i]['lng']))
+        temp.append(5)
+    worker_data.append(temp)
 
 for k,g in groupby(worker_data,lambda x:x[0]):
-	g = numpy.array(list(g))
-	g = sum(g[:,1])/(numpy.count_nonzero(g[:,1])+1)
-	temp = []
-	temp.append(k)
-	temp.append(g)
-	worker_smmry.append(temp)
+    g = numpy.array(list(g))
+    g = sum(g[:,1])/(numpy.count_nonzero(g[:,1])+1)
+    temp = []
+    temp.append(k)
+    temp.append(g)
+    worker_smmry.append(temp)
 
+print("Worker Data \n")
+print("###################################################################### \n")
+print(worker_data)
+print("###################################################################### \n")
 
 worker_smmry = sorted(worker_smmry,key=itemgetter(1))
+print("Worker Speed Summary data \n")
+print("###################################################################### \n")
 print(worker_smmry)
+print("###################################################################### \n")
+
+
 
 
 
